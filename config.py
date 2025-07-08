9d1c0eab212eeb8f2798a947cc6a3f1c1e4cb803
tiennm99 <tiennm99@outlook.com>
2025-07-08 09:45:31 +0700

    feat(proxy): add mtproxy support

--- config.py
+++ config.py
@@ -8,4 +8,8 @@ phone = os.getenv('PHONE')
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 
-group_names_to_export = ['sample1', 'sample2']  # Add the group names you want to export here
+proxy_host = os.getenv('PROXY_HOST')
+proxy_port = int(os.getenv('PROXY_PORT', '0')) if os.getenv('PROXY_PORT') else None
+proxy_secret = os.getenv('PROXY_SECRET')
+
+group_names_to_export = ['ZingPlay Game Studios', 'ZPS HCM', 'ZPS HCM - Xin nghỉ (phép/đi trễ)']  # Add the group names you want to export here
--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import getpass
 
 from telethon.errors import SessionPasswordNeededError
 from telethon.sync import TelegramClient
+import TelethonFakeTLS
 
 from common import *
 from config import *
 
-client = TelegramClient(phone, api_id, api_hash)
+if proxy_host and proxy_port and proxy_secret:
+    proxy = (proxy_host, proxy_port, proxy_secret)
+    connection = TelethonFakeTLS.ConnectionTcpMTProxyFakeTLS
+    client = TelegramClient(phone, api_id, api_hash, connection=connection, proxy=proxy)
+else:
+    client = TelegramClient(phone, api_id, api_hash)
+
 
 client.connect()
 if not client.is_user_authorized():
